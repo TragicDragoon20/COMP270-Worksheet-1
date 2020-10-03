cbe7bd6de53c37e5b623816cca66810177412694
Callum Metcalfe <callummgamedev@hotmail.com>
2020-10-03 19:33:16 +0100

    Added the function to draw the bezier curve

--- Bezier.cpp
+++ Bezier.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "Bezier.h"
+#include "math.h"
 
 Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3)
 	: p0(p0), p1(p1), p2(p2), p3(p3)
@@ -8,5 +9,24 @@ Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Ve
 
 void Bezier::draw(SDL_Renderer *renderer) const
 {
-	// TODO
+	const int numOfLineSegments = 20;
+	
+	Vector2 bezierPoints[numOfLineSegments];
+
+	for (float i = 0; i < numOfLineSegments; i++)
+	{
+
+		Vector2 pos[] =
+		{
+			bezierCurve(i / numOfLineSegments),
+			bezierCurve((i + 1) / numOfLineSegments)
+		};
+
+		SDL_RenderDrawLineF(renderer, pos[0].x, pos[0].y, pos[1].x, pos[1].y);
+	}
+}
+
+Vector2 Bezier::bezierCurve(float t) const
+{
+	return pow((1 - t), 3) * p0 + 3 * pow((1 - t), 2) * t * p1 + 3 * (1 - t) * pow(t, 2) * p2 + pow(t, 3) * p3;
 }
--- Bezier.h
+++ Bezier.h
@@ -6,9 +6,11 @@ class Bezier
 {
 public:
 	Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3);
+	Vector2 bezierCurve(float x) const;
 
 	void draw(SDL_Renderer *renderer) const;
-
 private:
 	Vector2 p0, p1, p2, p3;
+
+	
 };
